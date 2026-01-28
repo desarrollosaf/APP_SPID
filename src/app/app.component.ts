29d7da7ebeebbb54cc5cbe8c194065b78c2f2135
MarSGlez <martin.sanchezg@hotmail.com>
2026-01-28 14:20:41 -0600

    add cerrar sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from './service/user'
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -9,12 +12,20 @@ export class AppComponent {
   public appPages = [
     { title: 'Inicio', url: '/folder/inbox', icon: 'home' },
     { title: 'Sesion', url: '/sesiones', icon: 'paper-plane' },
-    { title: 'Cerrar sesion', url: '/auth/logout', icon: 'trash' },
+    { title: 'Cerrar sesion', action: 'logout', icon: 'trash' },
     // { title: 'Asuntos orden del dia', url: '/folder/favorites', icon: 'heart' },
     // { title: 'Orden del dia', url: '/folder/archived', icon: 'archive' },
     // { title: 'Marco Juridico', url: '/folder/trash', icon: 'trash' },
     // { title: 'Sesion', url: '/folder/spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor(private _userService: User, private router: Router) {}
+
+
+  logout() {
+    console.log('cerrar sesion')
+    localStorage.setItem('isLoggedin', 'false');
+    this.router.navigate(['/auth/login']);
+
+  }
 }
