709d031576718803c29068abf9be20b8eb525056
Nahum <flashjzd58@gmail.com>
2026-06-12 17:11:37 -0600

    cambios env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://parlamentario.congresoedomex.gob.mx/backend/api',
-  socketUrl: 'https://parlamentario.congresoedomex.gob.mx/backend'
+  socketUrl: 'https://parlamentario.congresoedomex.gob.mx',
+  socketPath: '/backend/socket.io'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3013/api',
-  socketUrl: 'http://localhost:3013'
+  socketUrl: 'http://localhost:3013',
+  socketPath: '/socket.io'
 };
 
 /*
